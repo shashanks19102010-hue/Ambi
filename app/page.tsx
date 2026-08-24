61f3bd61449d47da6cce6db6483e3134095be374
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-24 15:25:24 +0530

    feat: mount video generation bridge

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AmbiShell from "@/components/AmbiShell";
 import ImageGenerationBridge from "@/components/ImageGenerationBridge";
+import VideoGenerationBridge from "@/components/VideoGenerationBridge";
 
 export default function Home() {
-  return <><AmbiShell /><ImageGenerationBridge /></>;
+  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /></>;
 }
