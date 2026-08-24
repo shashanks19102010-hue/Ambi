877b005699352ac0a421c637e61ef17ce86033e9
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-24 15:29:04 +0530

    feat: mount audio generation bridge

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AmbiShell from "@/components/AmbiShell";
 import ImageGenerationBridge from "@/components/ImageGenerationBridge";
 import VideoGenerationBridge from "@/components/VideoGenerationBridge";
+import AudioGenerationBridge from "@/components/AudioGenerationBridge";
 
 export default function Home() {
-  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /></>;
+  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /><AudioGenerationBridge /></>;
 }
