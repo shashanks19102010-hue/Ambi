b6fb6f49c193ddd16e0761d2023f91052b771c50
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-26 14:06:19 +0530

    refactor: remove audio generation bridge

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import AmbiShell from "@/components/AmbiShell";
 import ImageGenerationBridge from "@/components/ImageGenerationBridge";
 import VideoGenerationBridge from "@/components/VideoGenerationBridge";
-import AudioGenerationBridge from "@/components/AudioGenerationBridge";
 
 export default function Home() {
-  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /><AudioGenerationBridge /></>;
+  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /></>;
 }
