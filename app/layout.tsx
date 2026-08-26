7682f6d19a12e1b2c0be410eb55ff96d899a592f
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-26 12:35:36 +0530

    style: load Ambi component refinement layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./ambi-calm.css";
+import "./ambi-calm-components.css";
 import PWARegister from "@/components/PWARegister";
 
 export const metadata: Metadata = {
