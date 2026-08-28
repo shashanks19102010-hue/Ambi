b37598355ea72abe54343314a081f22b2dda12dd
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-28 13:06:15 +0530

    refactor: remove Puter generation bridges

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import AmbiShell from "@/components/AmbiShell";
-import ImageGenerationBridge from "@/components/ImageGenerationBridge";
-import VideoGenerationBridge from "@/components/VideoGenerationBridge";
 import PexelsSearchBridge from "@/components/PexelsSearchBridge";
 
 export default function Home() {
-  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /><PexelsSearchBridge /></>;
+  return <><AmbiShell /><PexelsSearchBridge /></>;
 }
