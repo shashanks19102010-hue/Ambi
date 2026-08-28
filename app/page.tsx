4732a8bb63cc30aad86ec56eb337a7179c2f4c75
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-28 12:52:47 +0530

    feat: mount Pexels chat search

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AmbiShell from "@/components/AmbiShell";
 import ImageGenerationBridge from "@/components/ImageGenerationBridge";
 import VideoGenerationBridge from "@/components/VideoGenerationBridge";
+import PexelsSearchBridge from "@/components/PexelsSearchBridge";
 
 export default function Home() {
-  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /></>;
+  return <><AmbiShell /><ImageGenerationBridge /><VideoGenerationBridge /><PexelsSearchBridge /></>;
 }
