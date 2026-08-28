6a1af8bfc0583a07c2915cd054f2f937fa70e98c
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-08-28 20:02:51 +0530

    feat: mount automatic media generation bridge

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AmbiShell from "@/components/AmbiShell";
 import PexelsSearchBridge from "@/components/PexelsSearchBridge";
+import MediaGenerationBridge from "@/components/MediaGenerationBridge";
 
 export default function Home() {
-  return <><AmbiShell /><PexelsSearchBridge /></>;
+  return <><AmbiShell /><PexelsSearchBridge /><MediaGenerationBridge /></>;
 }
